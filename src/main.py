234b3f8fa38ff9211cb2bb4330e7532ca694b900
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-05-05 14:56:47 +0200

    Added registration and users fetching to client

--- src/main.py
+++ src/main.py
@@ -2,12 +2,15 @@ import socket
 import socks
 from _thread import start_new_thread
 from shared import read_data, service_setup
+import json
 
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150, True)
 HOST = "127.0.0.1"
 PORT = 5000
 CURRENT_MESSENGER = "other nick"
 USER_NICK = "my nick"
+SERVER_ADDRESS = "test"
+SERVICE_DIR = "<SRC DIR>"
 
 
 def messages_receiver(c: socket.socket):
@@ -26,7 +29,28 @@ def messages_sender(c: socket.socket):
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     service_setup(HOST, PORT, s)
-    # TODO: implement connection with server
+    with socks.socksocket() as ss:
+        ss.connect((f"{SERVER_ADDRESS}.onion", 5050))
+        register = input("Do you want to register? y/n")
+        while register != "n":
+            if register != "y":
+                print("Select y or n")
+                continue
+            nick = input("nick (can't contain white spaces): ")
+            if " " in nick:
+                print("CAN'T CONTAIN WHITE SPACES")
+                continue
+            address = open(f"{SERVICE_DIR}/hostname", "r").read().strip()
+            ss.sendall(f"NEW {nick} {address}".encode())
+            res = read_data(ss)
+            print(res)
+            if res == "registered":
+                break
+            input("Do you want to register? y/n")
+        ss.sendall("GET".encode())
+        users = json.loads(read_data(ss))
+        for user in users:
+            print(user)
     # TODO: implement connecting to others
     # TODO: implement receiving messages in the background
     # TODO: implement saving nicks and addresses
