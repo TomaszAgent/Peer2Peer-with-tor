a0ca4d47de65b80396d0d7024dcc6dad99a11bcc
Nokokiii <kubanokoki@gmail.com>
2024-05-11 02:36:42 +0200

    cleaning

--- src/server.py
+++ src/server.py
@@ -3,7 +3,6 @@ import socket
 import time
 from _thread import start_new_thread
 
-import click
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 
@@ -35,7 +34,6 @@ def handle_requests(client_socket: socket.socket, message: str) -> None:
     """
     This function is responsible for handling requests from the client that are not related to registering a user.
     """
-    print(message.split(maxsplit=4))
     match message.split()[0]:
         case "NEW":
             register_user(client_socket, message)
@@ -71,7 +69,7 @@ def register_user(client_socket: socket.socket, message: str) -> None:
             send_response(client_socket, "Nick is taken")
         else:
             users[nick] = (address, port, user_public_key)
-            print(f"User {nick} registered with address {address} and port {port}")
+            # print(f"User {nick} registered with address {address} and port {port}")
             send_response(client_socket, "User successfully registered")
             return
     send_response(client_socket, "Invalid registration request")
@@ -81,10 +79,8 @@ def register_user(client_socket: socket.socket, message: str) -> None:
 def connection_handler(client_socket: socket.socket) -> None:
     """
     This function is responsible for handling a connection with a client.
-    """
-    click.echo(f"New connection from {client_socket.getpeername()}")
-   
-    while True:
+    """   
+    while True: 
         message = read_data(client_socket)
         handle_requests(client_socket, message)
 
@@ -94,10 +90,10 @@ def user_printer() -> None:
     This function is reposible for printing the users connected to the server.
     """
     while True:
-        # clean_terminal()
-        # print(f"There is {len(users)} user(s) connected: ")
-        # for user in users:
-        #     print(f"{user}: {users[user]}")
+        clean_terminal()
+        print(f"There is {len(users)} user(s) connected: ")
+        for user in users:
+            print(f"{user}: {users[user][0]}:{users[user][1]}")
         time.sleep(1)
 
 
